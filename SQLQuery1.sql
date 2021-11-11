94a3116fe9a9886050ecdfa9a33b596db0124ca0
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-11 13:10:53 +0530

    [Srujan]Added code to create table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -5,3 +5,12 @@ Welcome to payroll service
 CREATE DATABASE payroll_service;
 USE payroll_service;
 
+--UC2--
+---Create table for DataBase---
+CREATE TABLE employee_payroll(
+id int not null,
+name varchar(20) not null,
+salary float not null,
+startdate date not null
+);
+
