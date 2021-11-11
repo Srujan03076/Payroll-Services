2b0407bf7a42c67332ff1383a8bfb8dadd5a32f6
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-11 13:19:38 +0530

    [Srujan]Added code to insert table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -14,3 +14,11 @@ salary money not null,
 startdate date not null
 );
 
+
+--UC3--
+---Insert table in database---
+INSERT INTO employee_payroll VALUES
+(1,'srujan',100000.00,'2021-04-03'),
+(2,'naresh',200000.00,'2020-05-22'),
+(3,'suresh',300000.00,'2019-06-19')
+
