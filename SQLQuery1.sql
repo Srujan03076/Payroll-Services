3265271ddf507154bf5071339f11cf421c9042e2
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-14 12:13:16 +0530

    [Srujan]Added code to alter new Gender column in existing table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -32,5 +32,9 @@ SELECT Name, Startdate FROM employee_payroll WHERE Name='srujan';
   SELECT * FROM employee_payroll WHERE StartDate between CAST('2019-01-01' as date) and GETDATE();
 
 
-
+  --------------Uc6------------------------
+-----------Alter the table to add gender---
+ALTER TABLE employee_payroll ADD Gender CHAR(1);
+UPDATE employee_payroll SET Gender='M' WHERE id=1;
+SELECT * FROM employee_payroll;
 
